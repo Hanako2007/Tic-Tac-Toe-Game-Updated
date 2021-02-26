e24586e1ddd0c4fbe94588361bd43e086c0250df
Hanako2007 <79155117+Hanako2007@users.noreply.github.com>
2021-02-26 15:42:14 -0600

    Added python file
    Added a new python program I coded to this repository.

---
+++ Tic Tac Toe Updated.py
@@ -0,0 +1,18 @@
+
+
+def createBoard(space):
+
+    
+    print(       '||'       '||'       );
+    print(       '||'    '  ||'  );
+    print(       '||'       '||'       );
+    print('===========================');
+    print(       '||'       '||'        );
+    print( space '||' space '||' space  );
+    print(       '||'       '||'        );
+    print('===========================');
+    print(       '||'       '||'        );
+    print( space '||' space '||' space  );
+    print(       '||'       '||'        );
+
+createBoard(0);
