dfe271aebf91f7614eb45a7f32313a8a564f0b82
Harya, Henry <henry.harya@wabtec.com>
2021-03-05 11:31:28 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# main.py
+
+# This file will be the entry point for your program.
+
+# To run it, run "python main.py" in your console.
+
+# Once you have renamed the other file, this will work below.
+import TicTacToe
+
+
+def main():
+    print('Welcome to Tic Tac Toe')
+    raise NotImplementedError('This function still has to be coded')
+    TicTacToe.start_game()
+
+
+if __name__ == '__main__':
+    main()
